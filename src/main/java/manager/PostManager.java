b0f8cd1e5745608aba86412159eb4d49450935cd
Alexey Potapenko <alesha_potapenko@mail.ru>
2021-09-03 19:30:47 +0300

    Post Manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,22 @@
+package manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    private Post[] posts;
+
+    public Post[] getPosts() {
+        return null;
+    }
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null; }
+
+    public void delete(int ownerId,int postId) {
+
+    }
+
+
+}
+
